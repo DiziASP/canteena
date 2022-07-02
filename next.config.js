6fe29b2e8a86d5cc94c0b4f971deaf82b8823600
Dizi <58800976+DiziASP@users.noreply.github.com>
2022-07-03 00:26:12 +0700

    fix: minor vercel bug

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ["picsum.photos", "firebasestorage.googleapis.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
